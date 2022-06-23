3530e5aefb7b3d50d1ec133d37a8365a1a186e70
Finn Thompson <79065282+18315ft@users.noreply.github.com>
2022-06-23 02:20:56 +0000

    Highlighted the current page on nav bar using css animations controlled by javascript

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function displayCurrentPage() {
   var dis = document.getElementById("main").scrollTop;
   // All of the page elements
   const pages = document.getElementsByTagName("section");
+  const icons = document.getElementsByClassName("navIcon");
 
   // Running through all the pages
   // If dis < the current pages height, that means that this is
@@ -19,10 +20,15 @@ function displayCurrentPage() {
   //   by 1/4 the pages height
   // The pages height is then taken away from displacement
   for (var x = 0; x < pages.length; x++) {
-    if (dis + window.innerHeight / 4 < pages[x].clientHeight) {
-      console.log(pages[x].id); //#FFA857
-      return;
+    if (dis + window.innerHeight / 4 < pages[x].clientHeight && dis + window.innerHeight / 4 > 0) {
+      // Sets the nav icon for the page to be a different colour. The x + 1 is because the
+      //  first icon is going to be the logo and so won't be highlighted
+      icons[x + 1].style.animationName = "fadeIn";
+    } else {
+      icons[x + 1].style.animationName = "fadeOut";
     }
+    
     dis -= pages[x].clientHeight;
   }
 }
+
